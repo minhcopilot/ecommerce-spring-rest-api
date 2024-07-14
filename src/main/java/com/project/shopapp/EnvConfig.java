abc964bb52fbe5089234fd51faff936612fc1a59
minhcopilot <21115053120333@sv.ute.udn.vn>
2024-07-14 23:27:40 +0700

    init project ecommerce-spring-rest-api

---
+++ src/main/java/com/project/shopapp/EnvConfig.java
@@ -0,0 +1,13 @@
+package com.project.shopapp;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class EnvConfig {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+    }
+}
